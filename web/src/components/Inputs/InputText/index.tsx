27c6d949e7c5b62956ca7c45a2ee69f0c3b33a81
marcoskloss <marcoskloss2001@gmail.com>
2021-08-12 18:52:10 -0300

    add error span in InputText component

--- web/src/components/Inputs/InputText/index.tsx
+++ web/src/components/Inputs/InputText/index.tsx
@@ -3,18 +3,25 @@ import {useForm} from "../../Form/Hooks/FormContext";
 
 interface IInputTextProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string
+  label: string
 }
 
-export function InputText({ name, ...rest }: IInputTextProps) {
-  const { register } = useForm()
+export function InputText({ name, label, ...rest }: IInputTextProps) {
+  const { register, errors } = useForm()
 
   const ref = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     register({ ref, name })
-  }, [])
+  }, [register, name])
 
   return (
-    <input id={name} ref={ref} {...rest} />
+    <>
+      <label htmlFor={name}>{ label }</label>
+      <input id={name} ref={ref} {...rest} />
+      { errors[name] && (
+        <span>{ errors[name] }</span>
+      )}
+    </>
   )
 }
--- web/todos.md
+++ web/todos.md
@@ -4,11 +4,12 @@ Some future improvements
 - [ ] New Component: Switcher
 - [ ] Theme Switcher button in AsideMenu
 - [ ] New Component: Header (for List and Details pages)
-- [ ] New Component: Form (for Details page)
+- [/] New Component: Form (for Details page)
 - [ ] New Component: RegistrationForm (for Login page)
 - [X] New Feat: Fake Server to CRUD fake data;
 - [X] Use the fake data from FakeServer in ui
 - [X] New Component: Form
+- [ ] Form: new funtion to clear form errors
 - [ ] New Feat: HTTPUtil - fetch wrapper
 - [ ] Improvement: Add 'error' flag in FakeServer CRUD methods to reject the Promise
 - [ ] Improvement: Refactor FakeServer methods to be able to handle different models and dont only Books
