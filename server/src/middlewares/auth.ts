7a70484c64361cf47706bddba657be4f8a40e271
marcoskloss <marcoskloss2001@gmail.com>
2021-09-05 21:44:35 -0300

    add auth middleware

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import AuthService from "src/services/auth";
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  const authHeader = req.headers?.['Authorization'];
+  if (!authHeader) return res.status(401).end();
+  const [, token] = String(authHeader).split(' ');
+
+  try {
+    const decoded = AuthService.decodeToken(token);
+    req.user_email = decoded.email;
+    next();
+  } catch (err: any) {
+    res.status(401).json({ error: err.message });
+  }
+}
