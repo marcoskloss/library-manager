8683339ac2bc23e0aa53bbb04b2193c53f8f63bf
marcoskloss <marcoskloss2001@gmail.com>
2021-09-18 11:09:45 -0300

    add jest setup

---
+++ server/jest.config.js
@@ -0,0 +1,31 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: [
+    'text',
+    'lcov'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  watchPathIgnorePatterns: ['node_modules'],
+  transformIgnorePatterns: ['node_modules'],
+  collectCoverageFrom: [
+    'src/**/*.ts', '!src/**/index.ts, !src/types.d.ts'
+  ]
+}
